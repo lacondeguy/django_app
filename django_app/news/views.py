f1ea3cd1301e3f4cd726828b42c66422661b0243
lacondeguy <ka4alin.alex@gmail.com>
2023-05-16 01:49:00 +0300

    add news app

--- django_app/news/views.py
+++ django_app/news/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def main(request):
+    return HttpResponse('Главная страница')
+
+def index(request):
+    return HttpResponse('Hello world!')
+
+def test(request):
+    return HttpResponse('<h1>Тестовая страница</h1>')
