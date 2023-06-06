52e5ceb617bdbcab47514584a1a0cbe7bcf3df00
lacondeguy <ka4alin.alex@gmail.com>
2023-06-06 22:23:05 +0300

    update template

--- django_app/news/urls.py
+++ django_app/news/urls.py
@@ -4,7 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
+    path('category/<int:category_id>/', get_category, name='category')
 ]
 
 if settings.DEBUG:
