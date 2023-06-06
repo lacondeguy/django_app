7d69f2e24d00c49c2e92d0eaf7dd13ed30520de8
lacondeguy <ka4alin.alex@gmail.com>
2023-06-07 00:21:53 +0300

    update template

--- django_app/news/views.py
+++ django_app/news/views.py
@@ -6,19 +6,15 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.order_by('-created_at')
-    categories = Category.objects.all()
-
     context = {
         'news_example': news,
         'title': 'Все новости',
-        'categories': categories,
     }
     return render(request, template_name='news/index.html', context=context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id = category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news_example': news, 'categories': categories, 'category': category })
+    return render(request, 'news/category.html', {'news_example': news, 'category': category})
 
